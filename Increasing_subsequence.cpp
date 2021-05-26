1e8420acf756e777c1c7941b458eee61627cd924
Nischey <heynischey@gmail.com>
2021-05-26 20:16:22 +0530

    lis

--- Increasing_subsequence.cpp
+++ Increasing_subsequence.cpp
@@ -2,27 +2,36 @@
 #include<iostream>
 using namespace std;
 
-int findans(int *copyarr,int n){
-    int *dp=new int [n];
-    dp[0]=1;
-    for(int i=1 ; i < n ; i++){
-        dp[i]=1;
-        for(int j=i-1 ; j >= 0 ; j--){
-            if(copyarr[i]>=copyarr[j]){
-                if(dp[j]>=dp[i]){
-                    dp[i]=dp[j]+1;
-                }
-            }
-        }
+int findans(int * arr,int n){
+    
+    if(n == 0){
+        return 0;
     }
 
-    int ans=0;
-    for(int i=0;i<n;i++){
-        if(dp[i]>ans){
-            ans=dp[i];
+    int * dp = new int [n+1];
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for(int i = 2 ; i <= n ; ++i){
+
+        dp[i] = 0;
+
+        for(int j = i-2 ; j >= 0 ; --j){
+
+            if(arr[j] < arr[i-1]){
+                dp[i] = max( 1 + dp[j+1] , dp[i]);
+            }
+
         }
+
     }
-    
+
+    int ans = 0 ;
+    for(int i = 1 ; i <= n ; ++i){
+        ans = max(ans,dp[i]);
+    }
+
+    delete []dp;
     return ans;
 
 }
